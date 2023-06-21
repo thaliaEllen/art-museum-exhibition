86efaacfa8614cbb9ebe30722c0e26f865b58ea0
thaliaEllen <thaliaellenaamaral@gmail.com>
2023-06-21 13:23:17 -0300

    feat: Apply function to change color of icon favorite on image gallery

--- src/components/galleryPhotos/imageToGallery.tsx
+++ src/components/galleryPhotos/imageToGallery.tsx
@@ -2,13 +2,23 @@ import { ThumbnailImageProps } from "react-grid-gallery";
 import { RiHeartFill } from "react-icons/ri";
 
 import "../../styles/imageToGallery.scss";
+import { useSelector } from "react-redux";
+import { useFavorites } from "../../redux/sliceFavorites";
 
 const ImageToGallery = (props: ThumbnailImageProps) => {
   const { src, alt, style, title } = props.imageProps;
+  const favoritesList = useSelector(useFavorites);
 
   return (
     <div>
-      <RiHeartFill className="imageToGallery" size={20} />
+      <RiHeartFill
+        className={
+          favoritesList.some((favorite) => favorite.id === props.item.key)
+            ? "imageToGalleryFavorite"
+            : "imageToGallery"
+        }
+        size={20}
+      />
       <img
         width={"100%"}
         alt={alt}
