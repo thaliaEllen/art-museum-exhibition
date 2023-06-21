a727c184d047fb3c99b86d1b7a37c7dca62f9718
thaliaEllen <thaliaellenaamaral@gmail.com>
2023-06-21 13:44:08 -0300

    fix: Move action favorite item to component imageToGallery

--- src/components/galleryPhotos/imageToGallery.tsx
+++ src/components/galleryPhotos/imageToGallery.tsx
@@ -1,17 +1,31 @@
 import { ThumbnailImageProps } from "react-grid-gallery";
 import { RiHeartFill } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
 
 import "../../styles/imageToGallery.scss";
-import { useSelector } from "react-redux";
-import { useFavorites } from "../../redux/sliceFavorites";
+import {
+  addFavorites,
+  removeSpecifcFavorite,
+  useFavorites,
+} from "../../redux/sliceFavorites";
 
 const ImageToGallery = (props: ThumbnailImageProps) => {
   const { src, alt, style, title } = props.imageProps;
   const favoritesList = useSelector(useFavorites);
+  const dispatch = useDispatch();
+
+  function manipulateFavorites() {
+    if (favoritesList.some((favorite) => favorite.id === props.item.key)) {
+      dispatch(removeSpecifcFavorite(props.item.key as number));
+    } else {
+      dispatch(addFavorites(props.item.key as number));
+    }
+  }
 
   return (
     <div>
       <RiHeartFill
+        onClick={() => manipulateFavorites()}
         className={
           favoritesList.some((favorite) => favorite.id === props.item.key)
             ? "imageToGalleryFavorite"
